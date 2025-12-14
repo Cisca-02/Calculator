31b42dd43e3bd0cc874d7a1622c3f954e59d9ffe
Cisca <233951544+CiscaSolana@users.noreply.github.com>
2025-12-14 10:00:42 +0100

    Add showOnDisplay func + fix html and css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//----BUSINESS LOGIC---
+
 //Function that adds two numbers
 const add = (a, b) => a + b;
 
@@ -14,21 +16,35 @@ const divide = (a,b) => a / b;
 //Function that do calculator's operations
 const operate = (num1, num2, operation) => {
     switch (operation){
-        //add
+
         case "+":
-            console.log(add(num1, num2));
-            break;
+            return add(num1, num2);
 
         case "-":
-            console.log(sub(num1, num2));
-            break;
+            return sub(num1, num2);
 
-        case "*":
-            console.log(multiply(num1, num2));
-            break;
+        case "X":
+            return multiply(num1, num2);
 
         case "/":
-            console.log(divide(num1, num2));
-            break;
+            return divide(num1, num2);
     }
 }
+
+
+//----DOM MANIPULATION----
+//In questa sezione inseriamo il codice che permette di modificare elementi del DOM a partire da input dati dall'utente
+//come ad esempio il caso di digitare dei bottoni
+
+//Obiettivo: quando l'utente digita dei numeri, questi vengono proiettati sul display
+//Funzione: prende in input un numero e lo aggiunge al paragrafo del display
+const showOnDisplay = n => {
+    //seleziono il paragrafo del display
+    const display = document.querySelector(".display-para");
+
+    //aggiungo l'elemento n al content del display
+    display.textContent += n;
+} 
+
+//test: 
+showOnDisplay("CIAO");
